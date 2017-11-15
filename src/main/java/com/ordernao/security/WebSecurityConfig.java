c3f99dfe715c23bc7848dc07408b11e770febade
shub121131 <shub.sharma8048@gmail.com>
2017-11-15 19:01:54 +0530

    CallNao live WAR_15_10_2017

---
+++ src/main/java/com/ordernao/security/WebSecurityConfig.java
@@ -0,0 +1,70 @@
+package com.ordernao.security;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+import com.ordernao.utility.OrderNaoConstants;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Autowired
+	private DataSource dataSource;
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+		/*.antMatchers(OrderNaoConstants.CALL_OPERATOR_LINKS).hasRole("CALLOPERATOR")
+		.antMatchers(OrderNaoConstants.MANAGER_LINKS).hasRole("MANAGER")
+		.antMatchers(OrderNaoConstants.DELIVERY_BOY_LINKS).hasRole("DELIVERYBOY")*/
+		.antMatchers("/").permitAll()
+		.antMatchers("/login").permitAll()
+		.antMatchers("/**").hasRole("ADMIN").and()
+		.formLogin().loginPage("/login").usernameParameter("username").passwordParameter("password").and()
+		.logout().logoutUrl("/logout")
+		.logoutSuccessUrl("/login?logout");
+
+		/*
+		 * http .authorizeRequests()
+		 * .antMatchers("/login").permitAll().and().formLogin().loginPage(
+		 * "/login")
+		 * .usernameParameter("username").passwordParameter("password").
+		 * permitAll().and().logout()
+		 * .logoutUrl("/logout").permitAll().logoutSuccessUrl("/login?logout");
+		 */
+
+		/*
+		 * http.authorizeRequests().antMatchers(OrderNaoConstants.
+		 * CALL_OPERATOR_LINKS).hasRole("CALL OPERATOR");
+		 * http.authorizeRequests().antMatchers(OrderNaoConstants.MANAGER_LINKS)
+		 * .hasRole("MANAGER");
+		 * http.authorizeRequests().antMatchers(OrderNaoConstants.
+		 * DELIVERY_BOY_LINKS).hasRole("DELIVERY BOY");
+		 * http.authorizeRequests().antMatchers("/**").hasRole("ADMIN");
+		 */
+
+		// Disable csrf for now
+		http.csrf().disable();
+	}
+
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+		auth.jdbcAuthentication().dataSource(dataSource)
+				.usersByUsernameQuery("select email as username,password,true as enabled from users where email = ?")
+				.authoritiesByUsernameQuery(
+						"select email as username,concat(\"ROLE_\",role_name) as role from users inner join roles on users.roleid = roles.role_id where email = ?");
+	}
+
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring().antMatchers("/resources/**", "/js/**", "/css/**", "/images/**"); // #3
+	}
+}
